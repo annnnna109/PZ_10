73b6c41c822240ee4686f90e2a1012d95805f8bb
annnnna109 <144040921+annnnna109@users.noreply.github.com>
2023-11-22 20:14:14 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Инициализация двумерного массива слов
+            string[,] wordsArray = new string[,]
+          { { "Города:", "Страны:", "Океаны:" },
+            { "Уфа", "Россия", "Тихий" },
+            { "Оренбург", "Корея", "Атлантический" }
+            };
+            // Получение размерности массива
+            int rows = wordsArray.GetLength(0);
+            int columns = wordsArray.GetLength(1);
+            // Создание и заполнение текстового массива
+            string[] textArray = new string[columns];
+            for (int j = 0; j < columns; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    textArray[j] += wordsArray[i, j] + " ";
+                }
+            }
+            // Вывод полученного текста
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine(textArray[j]);
+
+            }
+        }
+    }
+}
